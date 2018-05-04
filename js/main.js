f4178bafe96563d5fa82c0c9ef4bd7f7205a0b06
milena-mica <trajkovic.milena.mica@gmail.com>
2018-05-04 13:33:39 +0200

    Iphone Animation

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready (function() {
+
+     $('.js-wp-2').waypoint(function(direction) {
+
+        $('.js-wp-2').addClass('animated slideInUp');
+
+     }, {
+         offset: '70%'
+     });
+
+
+
+});
